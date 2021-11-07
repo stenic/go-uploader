b6c78100ebde4e886859ecec41e792b65bdd65ca
Dries De Peuter <dries@stenic.io>
2021-11-07 02:07:01 +0100

    ci: Enable pipeline

--- release.config.js
+++ release.config.js
@@ -15,7 +15,7 @@ module.exports = {
 	  }],
 	  ["@semantic-release/github", {
 		"assets": [
-		  {"path": "dist/*.zip", "label": "Binary distribution"}
+		  {"path": "dist/*.zip"}
 		]
 	  }],
 	],
